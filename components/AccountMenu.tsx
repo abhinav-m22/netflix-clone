e6af6d77a2f2b26ebaccc9996ed9771e56c1ac9a
abhinav-m22 <abhinavpm05@gmail.com>
2023-03-08 17:46:18 +0530

    deployment bugs

--- components/AccountMenu.tsx
+++ components/AccountMenu.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { signOut } from 'next-auth/react';
-import useCurrentUser from '@/hooks/useCurrentUser';
+import useCurrentUser from "@/hooks/useCurrentUser";
 
 interface AccountMenuProps {
     visible?: boolean;
@@ -8,17 +8,17 @@ interface AccountMenuProps {
 
 const AccountMenu: React.FC<AccountMenuProps> = ({visible}) => {
 
-    if(!visible) return null;
+    const{ data: currentUser } = useCurrentUser();
 
-    const { data } = useCurrentUser();
+    if(!visible) return null;
 
   return (
     <div className='bg-black w-56 absolute top-14 right-0 py-4 flex-col border-gray-800 flex'>
         <div className='flex flex-col gap-3'>
-            <div className='px-3 group/item flex flex-row gap-3 items-center w-full items-center justify-center'>
+            <div className='px-3 group/item flex flex-row gap-3 w-full items-center justify-center'>
                 <img className='w-8 rounded-md' src="/images/green-profile-img.png" alt="" />
                 <p className='text-white text-sm group-hover/item:underline font-semibold'>
-                    {data?.name}
+                    {currentUser?.name}
                 </p>
             </div>
             <hr className='bg-gray-600 border-0 h-px my-1'/>
