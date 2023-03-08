a1150a07d186fabb32c33cdfa795099f919ea62e
abhinav-m22 <abhinavpm05@gmail.com>
2023-03-08 12:24:51 +0530

    Video player route

--- components/Billboard.tsx
+++ components/Billboard.tsx
@@ -2,6 +2,7 @@ import useBillboard from '@/hooks/useBillboard'
 import React from 'react'
 
 import { AiOutlineInfoCircle } from 'react-icons/ai';
+import PlayButton from './PlayButton';
 
 const Billboard = () => {
     const { data } = useBillboard();
@@ -32,6 +33,9 @@ const Billboard = () => {
             </p>
 
             <div className='flex flex-row items-center mt-3 md:mt-4 gap-3'>
+
+                <PlayButton movieId={data?.id} />
+
                 <button className='
                 text-white
                 bg-white
--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { BsFillPlayFill } from 'react-icons/bs';
 import FavoriteButton from './FavoriteButton';
+import { useRouter } from 'next/router';
 
 interface MovieCardProps {
     data: Record<string, any>
 }
 
 const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
+
+    const router = useRouter();
+
     return (
         <div className='group bg-zinc-900 col-span relative h-[12vw]'>
             <img
@@ -84,7 +88,7 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
                     transition
                     hover:bg-neutral-300
                 '
-                            onClick={() => { }}
+                onClick={() => router.push(`/watch/${data?.id}`)}
                         >
                             < BsFillPlayFill size={30} />
                         </div>
